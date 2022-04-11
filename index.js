3406196946f2bfe66034ec5320fb481692d4d4fc
Nilton <niltonroese@gmail.com>
2022-04-11 18:12:17 +1000

    API fetch done

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ function artistLink(url){
     window.open(url);
 }
 
-/*Search Button*/
+/*Genre Buttons*/
 
-document.getElementById('search-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const band = document.getElementById('band-input').value
-  // console.log(band);
-  window.open('https://google.com', '_blank');
-})
+function musicGenre(overview){
+  const x = document.getElementById(overview);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
-/*API Fetch*/
+/*Search Button and Fetch*/
+
+// document.getElementById('search-form').addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const band = document.getElementById('band-input').value
+//   console.log(band);
+//   window.open('https://google.com', '_blank');
+// })
 
 const options = {
 	method: 'GET',
@@ -23,21 +32,47 @@ const options = {
 	}
 };
 
-async function fetchKingstonTrioAsync() {
-    return fetch('https://deezerdevs-deezer.p.rapidapi.com/artist/17228', options)
-}
+document.getElementById('search-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const bandValue = document.getElementById('band-input').value
+  fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q='+bandValue, options)
+  // console.log(bandValue);
+  .then(resp => resp.json())
+  // .then(json => console.log(json))
+  .then(json => {
+    const bandLink = json.data[0]['artist']['link'];
+    // console.log(bandLink);
+    window.open(bandLink, '_self');
+  })
+  .catch(error => window.alert('There is no such band with this name!'));  
+})
 
-async function KingstonTrio(){
-//     // async await modern js
-    try{
-        const resp = await fetchKingstonTrioAsync();
-        const json = await resp.json();
-        console.log(json);
-        window.alert(json.link);
-    } catch(error) {
-        window.alert('error');  
-    }
-}
+/*Fetch Neil Young link*/
+// async function fetchneilYoungAsync() {
+// return fetch('https://deezerdevs-deezer.p.rapidapi.com/artist/1379', options)
+// }
+
+// async function neilYoung(){
+//   // async await modern js
+//   try {
+//       const resp = await fetchneilYoungAsync();
+//       const json = await resp.json();
+//       console.log(json);
+//       window.alert(json.link);
+//     } catch(error) {
+//       window.alert('error');  
+//   }
+// }
+
+/*API Fetch*/
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+// 		'X-RapidAPI-Key': '500ea0140bmshf6f2c9a6da4a206p1519d9jsn9fc495bd022b'
+// 	}
+// };
 
 // async function fetchBobDAsync() {
 //   return fetch('https://deezerdevs-deezer.p.rapidapi.com/artist/808', options)
@@ -49,7 +84,7 @@ async function KingstonTrio(){
 //       const resp = await fetchBobDAsync();
 //       const json = await resp.json();
 //       console.log(json);
-//       //window.alert(json.link);
+//       window.alert(json.link);
 
 //       let container = document.getElementById('bobDylanContainer');
 
@@ -65,221 +100,3 @@ async function KingstonTrio(){
 //       window.alert('error');  
 //   }
 // }
-
-/*Genre Buttons*/
-
-  // function jazz(); blues(); randB(); {
-  //   const x = document.querySelectorAll("#jazzOverview", "#bluesOverview", "#randBOverview");
-  //   if (x.style.display === "none") {
-  //     x.style.display = "block";
-  //   } else {
-  //     x.style.display = "none";
-  //   }
-  // }
-
-  function blues(){
-    const x = document.getElementById("bluesOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function randB(){
-    const x = document.getElementById("randBOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function folk(){
-    const x = document.getElementById("folkOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function folkRock(){
-    const x = document.getElementById("folkRockOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function rockabilly(){
-    const x = document.getElementById("rockabillyOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function britInvasion(){
-    const x = document.getElementById("britInvasionOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function soul(){
-    const x = document.getElementById("soulOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function psychedelicRock(){
-    const x = document.getElementById("psychedelicOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function progRock(){
-    const x = document.getElementById("progRockOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function experimentalRock(){
-    const x = document.getElementById("experimentalOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function southernRock(){
-    const x = document.getElementById("southernOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function glitterRock(){
-    const x = document.getElementById("glitterOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function hardRock(){
-    const x = document.getElementById("hardRockOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function punk(){
-    const x = document.getElementById("punkOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function grunge(){
-    const x = document.getElementById("grungeOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function heavyMetal(){
-    const x = document.getElementById("heavyMetalOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function popRock(){
-    const x = document.getElementById("popRockOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function newWave(){
-    const x = document.getElementById("newWaveOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function dooWop(){
-    const x = document.getElementById("dooWopOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function funk(){
-    const x = document.getElementById("funkOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function disco(){
-    const x = document.getElementById("discoOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function rap(){
-    const x = document.getElementById("rapOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function hiphop(){
-    const x = document.getElementById("hiphopOverview");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
