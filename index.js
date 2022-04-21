1a8629f6de387b0e5c2d0b1d93a625a84c0a457a
Nilton <niltonroese@gmail.com>
2022-04-21 18:46:47 +1000

    WIP

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function musicGenre(overview){
   }
 }
 
-/*Search Button and Fetch*/
+/*Search Button and API Fetch*/
 
 // document.getElementById('search-form').addEventListener('submit', (e) => {
 //   e.preventDefault();
@@ -47,7 +47,7 @@ document.getElementById('search-form').addEventListener('submit', (e) => {
   .catch(error => window.alert('There is no such band with this name!'));  
 })
 
-/*Fetch Neil Young link*/
+/*Fetch Neil Young link try catch await async function*/
 // async function fetchneilYoungAsync() {
 // return fetch('https://deezerdevs-deezer.p.rapidapi.com/artist/1379', options)
 // }
@@ -64,39 +64,27 @@ document.getElementById('search-form').addEventListener('submit', (e) => {
 //   }
 // }
 
-/*API Fetch*/
+/*Pop-up / Favorite band storing*/
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
-// 		'X-RapidAPI-Key': '500ea0140bmshf6f2c9a6da4a206p1519d9jsn9fc495bd022b'
-// 	}
-// };
+function favoriteArtist(name, favoriteBand) {
+  return fetch('http://localhost:3000/favoriteArtist',{
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        favoriteBand
+      })
+    })
+    .then(resp => resp.json())
+    .then(data=> {
+      return data
+    })
+    .catch(error => console.error(error));
+}
 
-// async function fetchBobDAsync() {
-//   return fetch('https://deezerdevs-deezer.p.rapidapi.com/artist/808', options)
-// }
-
-// async function BobD(){
-//   // async await modern js
-//   try{
-//       const resp = await fetchBobDAsync();
-//       const json = await resp.json();
-//       console.log(json);
-//       window.alert(json.link);
-
-//       let container = document.getElementById('bobDylanContainer');
-
-//       if(document.querySelectorAll("#artist-link").length === 0){
-//           let url = document.createElement('a');
-//           url.id = 'artist-link'
-//           url.href = json.link;
-//           url.target = '_blank'
-//           url.innerHTML = json.link;
-//           container.append(url);
-//       }
-//   } catch(error) {
-//       window.alert('error');  
-//   }
-// }
+function hide() {  
+  document.getElementById('popup').style.display = "none";
+};
